3410c801c8ea709c66339671cb854801c9c86f7e
SahilKhutey <SahilKhutey@example.com>
2026-08-07 14:12:27 +0530

    chore(clean): remove build cache and dist-release-2.0.0 from git tracking

--- build/itch-release/Examples/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ASUS/Documents/Primodilas/Examples
-# Build directory: C:/Users/ASUS/Documents/Primodilas/build/itch-release/Examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("multiplayer_demo")
-subdirs("scripted_demo")
--- build/itch-release/Tools/localization/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ASUS/Documents/Primodilas/Tools/localization
-# Build directory: C:/Users/ASUS/Documents/Primodilas/build/itch-release/Tools/localization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
